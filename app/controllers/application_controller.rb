8ead651eb82dab62b1f4de477739935dd1e3d238
EC2 Default User <ec2-user@ip-172-31-0-179.ec2.internal>
2022-01-23 10:40:19 +0000

    ログアウトエラーの修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!,except: [:top, :about]
 
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
-    user_path(resource)
+    root_path
   end
 
 
