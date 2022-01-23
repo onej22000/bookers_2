3cdcd7b2960456cca95cc5cbdafc74b7c11aa024
EC2 Default User <ec2-user@ip-172-31-0-179.ec2.internal>
2022-01-23 05:36:07 +0000

    サインイン時のパス追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
     user.update(user_params)
-    redirect_to user_path(user.id)
+    redirect_to book_path(user.id)
   end
 
   private
